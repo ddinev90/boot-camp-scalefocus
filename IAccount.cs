ff5331b8a15e860be3b7db22e06a461730acc056
Daniel Dinev <daniel.dinev@scalefocus.com>
2018-04-11 14:00:44 +0300

    created interface for basic operations for future dev

---
+++ IAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BootCampBank
+{
+    public interface IAccount
+    {
+
+        double Deposit();
+        double Withdraw();
+        double Transfer();
+        void ShowInfo();
+
+    }
+}
